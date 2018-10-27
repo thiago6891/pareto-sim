50c8742ea04816dda4a8dd9affac6332f97d18f2
Thiago Cordeiro <thiago6891@gmail.com>
2018-10-27 10:58:51 -0300

    pareto simulation done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Simulation from './Simulation';
+import WebWorker from './WebWorker';
+import { LineChart, Line, XAxis, YAxis } from 'recharts';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = { arr: [], iteration: 0 };
+    }
+
+    componentDidMount() {
+        this._worker = new WebWorker(Simulation);
+
+        this._worker.onmessage = (e) => {
+            if (!e) return;
+
+            this.setState({
+                arr: e.data.data
+                    .map(x => {
+                        return {name: '', uv: x};
+                    }),
+                iteration: e.data.iteration
+            });
+        };
+    }
+
+    componentWillUnmount() {
+        this._worker.terminate();
+    }
+
+    startSimulation() {
+        this._worker.postMessage('start');
+    }
+    
+    render() {
+        return (
+            <div className="App">
+                <div className='App-header'>
+                    <LineChart width={800} height={400} data={this.state.arr}>
+                        <XAxis dataKey='name' />
+                        <YAxis />
+                        <Line type='monotone' dataKey='uv' stroke='#8884D8' />
+                    </LineChart>
+                    <p>Iteration: { this.state.iteration }</p>
+                    <button onClick={() => this.startSimulation()}>Start</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Simulation.js
@@ -0,0 +1,41 @@
+export default () => {
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    const iterations = 1000000000;
+    const arr = Array(1000).fill(100);
+
+    var simulationStarted = false;
+
+    postMessage({ data: arr, iteration: 0 });
+
+    onmessage = e => {
+        if (simulationStarted || !e || e.data !== 'start') return;
+
+        simulationStarted = true;
+
+        for (let i = 0; i < iterations; i++) {
+            let r1, r2;
+            
+            do {
+                r1 = getRandomInt(arr.length);
+            } while (arr[r1] === 0);
+            
+            do {
+                r2 = getRandomInt(arr.length);
+            } while (r1 === r2);
+    
+            arr[r1] -= 1;
+            arr[r2] += 1;
+    
+            if (i % 100000 === 0) {
+                let data = arr.slice();
+                data.sort((a, b) => {
+                    return b - a;
+                });
+                postMessage({ data: data, iteration: i });
+            }
+        }
+    };
+};
